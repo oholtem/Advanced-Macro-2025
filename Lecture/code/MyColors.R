88ed6837a35e5bf195490b81ac60806785504142
Oliver Holtemoeller <62705093+oholtem@users.noreply.github.com>
2025-10-15 15:37:50 +0200

    MyColors outsourced

---
+++ Lecture/code/MyColors.R
@@ -0,0 +1,9 @@
+# Definition of Colors
+BfBlack         <- rgb(  0,   0,   0, maxColorValue = 255)
+BfOrange        <- rgb(230, 159,   0, maxColorValue = 255)
+BfSkyBlue       <- rgb( 86, 180, 233, maxColorValue = 255)
+BfBluishGreen   <- rgb(  0, 158, 115, maxColorValue = 255)
+BfYellow        <- rgb(240, 228,  66, maxColorValue = 255)
+BfBlue          <- rgb(  0, 114, 178, maxColorValue = 255)
+BfVermillon     <- rgb(213,  94,   0, maxColorValue = 255)
+BfReddishPurple <- rgb(204, 121, 167, maxColorValue = 255)
